a98f28b6bf1d23b42362fe2407290f14ca07cad9
PratikHajare <66741672+HajarePratik@users.noreply.github.com>
2021-06-20 10:15:34 +0530

    Validate & Test User Last Name

--- UserRegistrationTest.java
+++ UserRegistrationTest.java
@@ -8,19 +8,19 @@ public class UserRegistrationTest {
 	
 	// Test 1 : First name starts with capital letter and has minimum 3 characters
 	@Test
-	public void Testing_validatefirstname() 
+	public void testing_validatefirstname() 
 	{
-		UserRegistrationForTestcase firstname = new UserRegistrationForTestcase();
-		boolean result = firstname.isvalidfirstname("Pratik");
+		UserRegistrationForTestcase user = new UserRegistrationForTestcase();
+		boolean result = user.isvalidfirstname("Pratik");
 		Assert.assertEquals(true, result);
 	}
 
 	// Test 2 : Last name starts with capital letter and has minimum 3 characters
 	@Test
-	public void Testing_validatelastname() 
+	public void testing_validatelastname() 
 	{
-		UserRegistrationForTestcase lastname = new UserRegistrationForTestcase();
-		boolean result = lastname.isvalidlastname("Hajare");
+		UserRegistrationForTestcase user = new UserRegistrationForTestcase();
+		boolean result = user.isvalidlastname("Hajare");
 		Assert.assertEquals(true, result);
 	}
 	
