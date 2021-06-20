d5d037c98b0dbb8272d832375feaeca849da44cb
PratikHajare <66741672+HajarePratik@users.noreply.github.com>
2021-06-20 10:14:43 +0530

    Validate & Test User Last Name

--- UserRegistrationForTestcase.java
+++ UserRegistrationForTestcase.java
@@ -5,28 +5,27 @@ import java.util.regex.Pattern;
 
 public class UserRegistrationForTestcase {
 	
+	Pattern pattern;
+	Matcher match;
+	
 	// UC1 Method to validate the firstname
 	public boolean isvalidfirstname(String firstname) 
 	{
-		Pattern p;
-		Matcher m;
 		//Regex to check valid firstname
 		String  regex = "^[A-Z]{1}[a-z]{2,}";
-		p = Pattern.compile(regex);
-		m = p.matcher(firstname);
-		return m.matches();
+		pattern = Pattern.compile(regex);
+		match = pattern.matcher(firstname);
+		return match.matches();
 	}
 	
 	// UC2 Method to validate the lastname
 	public boolean isvalidlastname(String lastname) 
 	{
-		Pattern p;
-		Matcher m;
 		//Regex to check valid lastname
 		String  regex = "^[A-Z]{1}[a-z]{2,}";
-		p = Pattern.compile(regex);
-		m = p.matcher(lastname);
-		return m.matches();
+		pattern = Pattern.compile(regex);
+		match = pattern.matcher(lastname);
+		return match.matches();
 	}
 	
 }
